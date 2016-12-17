447f74e8f7bceb3d46d68dbcd4829e33face9435
Armand <hoel@free.fr>
2016-12-17 17:18:34 +0100

    parallellisme génération cases voisines

--- vaoc/FormCarte.cs
+++ vaoc/FormCarte.cs
@@ -11,6 +11,8 @@ using System.IO;
 using System.Diagnostics;
 using Microsoft.Win32;
 using WaocLib;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace vaoc
 {
@@ -24,6 +26,7 @@ namespace vaoc
         private Donnees.TAB_CASEDataTable               m_tableCase;
         private Donnees.TAB_CASERow[]                   m_tableCaseTraitement;
         private DateTime                                m_dateDebut;
+        private List<Task> m_tasks = new List<Task>();
         #endregion
 
         #region propriétés
@@ -169,6 +172,9 @@ namespace vaoc
                 case 1:
                     //on parcours toutes les cases pour déterminer les cases de remplacement (cas des routes)
                     //on optimise les traitements en metteant les cases en mémoire
+
+                    /* Performance, sur une petite carte 1152x648 : tps de baser: 3 heures, solution 1 : 2 minutes, solution 2: 13 secondes */
+                    /* Solution 1 :
                     if (0 == m_sous_traitement)
                     {
                         tableCase.InitialisationListeCase(m_imageCarte.Width, m_imageCarte.Height);//optimisation mémoire
@@ -176,29 +182,74 @@ namespace vaoc
                         //tableCase.AcceptChanges();//on valide les ajouts du traitement précédent
                         labelTraitement.Text = "cases de remplacement";
                         m_tableCaseTraitement = (Donnees.TAB_CASERow[])m_tableCase.Select("ID_MODELE_TERRAIN_SI_OCCUPE IS NULL");
+                        //progressBar.Maximum = m_imageCarte.Width;//m_tableCaseTraitement.Length;
                         progressBar.Maximum = m_tableCaseTraitement.Length;
                         m_dateDebut = DateTime.Now;
                         buttonGeneration.Text = "Arrêt";
                     }
-
                     if (m_sous_traitement < m_tableCaseTraitement.Length)
                     {
-                        //CreationsVoisins(m_tableCase[m_sous_traitement]); -> inutile a priori, facile a déduire durant le traitement
-                        CreationCasesDeRemplacement(m_tableCaseTraitement[m_sous_traitement]);
                         if (0 == m_sous_traitement % 100)
                         {
-                            Debug.WriteLine("");
                             progressBar.Value = m_sous_traitement;
                             AfficherTemps();
                             Invalidate();
                         }
-                        m_sous_traitement++;
+                        m_sous_traitement += TraitementParallele();// TraitementMono();
+                        //m_sous_traitement += TraitementMono();
                     }
                     else
                     {
+                        //controle que tout a bien marche
+                        for (int i = 0; i < m_tableCaseTraitement.Length; i++)
+                        {
+                            if (m_tableCaseTraitement[i].IsID_MODELE_TERRAIN_SI_OCCUPENull())
+                            {
+                                Debug.WriteLine(string.Format("IsID_MODELE_TERRAIN_SI_OCCUPENull pour X={0},Y={1},ID={2}", m_tableCaseTraitement[i].I_X, m_tableCaseTraitement[i].I_Y, m_tableCaseTraitement[i].ID_CASE));
+                            }
+                        }
+                        m_sous_traitement = 0;
+                        m_traitement++;
+                    }                    
+                    */
+                    /* Solution 2 */
+                    if (0 == m_sous_traitement)
+                    {
+                        tableCase.InitialisationListeCase(m_imageCarte.Width, m_imageCarte.Height);//optimisation mémoire
+                        //AcceptChanges only updates your rows in the (in memory) dataset, that is - marks them as "not needed for actual database update".
+                        //tableCase.AcceptChanges();//on valide les ajouts du traitement précédent
+                        labelTraitement.Text = "cases de remplacement";
+                        m_tableCaseTraitement = (Donnees.TAB_CASERow[])m_tableCase.Select("ID_MODELE_TERRAIN_SI_OCCUPE IS NULL");
+                        progressBar.Maximum = m_imageCarte.Width;//m_tableCaseTraitement.Length;
+                        m_dateDebut = DateTime.Now;
+                        buttonGeneration.Text = "Arrêt";
+                    }
+                    if (m_sous_traitement < m_imageCarte.Width)
+                    {
+                        //CreationsVoisins(m_tableCase[m_sous_traitement]); -> inutile a priori, facile a déduire durant le traitement
+                        if (0 == m_sous_traitement % 100)
+                        {
+                            progressBar.Value = m_sous_traitement;
+                            AfficherTemps();
+                            Invalidate();
+                        }
+                        //m_sous_traitement += TraitementParallele();// TraitementMono();
+                        m_sous_traitement += TraitementParallele2(m_imageCarte.Height);
+                    }
+                    else
+                    {
+                        //controle que tout a bien marche
+                        for (int i=0; i<m_tableCaseTraitement.Length;i++)
+                        {
+                            if (m_tableCaseTraitement[i].IsID_MODELE_TERRAIN_SI_OCCUPENull())
+                            {
+                                Debug.WriteLine(string.Format("IsID_MODELE_TERRAIN_SI_OCCUPENull pour X={0},Y={1},ID={2}", m_tableCaseTraitement[i].I_X, m_tableCaseTraitement[i].I_Y, m_tableCaseTraitement[i].ID_CASE));
+                            }
+                        }
                         m_sous_traitement = 0;
                         m_traitement++;
                     }
+                     /* */
                     break;
                 case 3:
                     //on met seulement à jour les modeles de terrain suite à une modification mineure de l'image source
@@ -240,6 +291,45 @@ namespace vaoc
                     return false;//traitement terminé
             }
             return true;
+        }
+
+        private int TraitementParallele2(int nbPointsParTraitement)
+        {
+            int nbTasks = Math.Min(100, m_imageCarte.Width - m_sous_traitement); // pas plus de 100 tâches à la fois
+            for (int k = 0; k < nbTasks; k++)
+            {
+                //m_sous_traitement++;
+                //m_tasks.Add(Task.Run(() =>
+                //int debut = k + m_sous_traitement;
+                m_tasks.Add(Task.Factory.StartNew( (debut)=>
+                {
+                    Debug.WriteLine("TraitementParallele2 m_sous_traitement=" + ((int)debut).ToString());
+                    for (int i = 0; i < nbPointsParTraitement; i++)
+                    { CreationCasesDeRemplacement(m_tableCaseTraitement[((int)debut * nbPointsParTraitement) + i]); }
+                }, k + m_sous_traitement));
+            }
+            Task.WaitAll(m_tasks.ToArray());
+            return nbTasks; //-> déjà fait dans m_sous_traitement++, pas vrai, ne change rien du tout à la valeur !!!
+        }
+
+        private int TraitementParallele()
+        {
+            int nbTasks = Math.Min(100, m_tableCaseTraitement.Length - m_sous_traitement); // pas plus de 100 tâches à la fois
+            for (int k = 0; k < nbTasks; k++)
+            {
+                m_tasks.Add(Task.Factory.StartNew((debut) =>
+                {
+                    CreationCasesDeRemplacement(m_tableCaseTraitement[(int)debut]);
+                }, k + m_sous_traitement));
+            }
+            Task.WaitAll(m_tasks.ToArray());
+            return nbTasks;
+        }
+
+        private int TraitementMono()
+        {
+            CreationCasesDeRemplacement(m_tableCaseTraitement[m_sous_traitement]);
+            return 1;
         }
 
         private void AfficherTemps()
@@ -385,53 +475,58 @@ namespace vaoc
             int                     valeur,maximum;
             Int32                   modeleCible;
 
-               SortedList<Int32, int> liste = new SortedList<Int32, int>();
+            //-> Cela peut aussi créer des conflits d'accès... Debug.WriteLine(string.Format("CreationCasesDeRemplacement: ID_CASE={0}, X={1}, Y={2}",ligneCase.ID_CASE, ligneCase.I_X, ligneCase.I_Y));
+            SortedList<Int32, int> liste = new SortedList<Int32, int>();
 
-               //analyse de toutes les cases voisines pour trouver le modèle de terrain approprié
-               foreach (Donnees.TAB_CASERow ligneVoisin in tableCase.CasesVoisines(ligneCase))
-               {
-                   //si la case voisine est elle-même inutilisable si occupée, on n'en tient pas compte
-                   Donnees.TAB_MODELE_TERRAINRow ligneModeleTerrainVoisin = Donnees.m_donnees.TAB_MODELE_TERRAIN.FindByID_MODELE_TERRAIN(ligneVoisin.ID_MODELE_TERRAIN);
-                   if (null == ligneModeleTerrainVoisin)
-                   {
-                       string message = string.Format("CreationCasesDeRemplacement ne trouve pas de modèle de terrain pour la case voisine id={0} x={1} et y={2}",
-                           ligneVoisin.ID_MODELE_TERRAIN, ligneVoisin.I_X, ligneVoisin.I_Y);
-                       throw new Exception(message);
-                   }
-                   if (false == ligneModeleTerrainVoisin.B_ANNULEE_SI_OCCUPEE)
-                   {
-                       if (liste.TryGetValue(ligneVoisin.ID_MODELE_TERRAIN, out valeur))
-                       {
-                           liste[ligneVoisin.ID_MODELE_TERRAIN] = ++valeur;
-                       }
-                       else
-                       {
-                           liste.Add(ligneVoisin.ID_MODELE_TERRAIN,1);
-                       }                       
-                   }
-               }
+            //analyse de toutes les cases voisines pour trouver le modèle de terrain approprié
+            Monitor.Enter(Donnees.m_donnees.TAB_MODELE_TERRAIN);
+            foreach (Donnees.TAB_CASERow ligneVoisin in tableCase.CasesVoisines(ligneCase))
+            {
+                //si la case voisine est elle-même inutilisable si occupée, on n'en tient pas compte
+                Donnees.TAB_MODELE_TERRAINRow ligneModeleTerrainVoisin = Donnees.m_donnees.TAB_MODELE_TERRAIN.FindByID_MODELE_TERRAIN(ligneVoisin.ID_MODELE_TERRAIN);
+                if (null == ligneModeleTerrainVoisin)
+                {
+                    string message = string.Format("CreationCasesDeRemplacement ne trouve pas de modèle de terrain pour la case voisine id={0} x={1} et y={2}",
+                        ligneVoisin.ID_MODELE_TERRAIN, ligneVoisin.I_X, ligneVoisin.I_Y);
+                    throw new Exception(message);
+                }
+                if (false == ligneModeleTerrainVoisin.B_ANNULEE_SI_OCCUPEE)
+                {
+                    if (liste.TryGetValue(ligneVoisin.ID_MODELE_TERRAIN, out valeur))
+                    {
+                        liste[ligneVoisin.ID_MODELE_TERRAIN] = ++valeur;
+                    }
+                    else
+                    {
+                        liste.Add(ligneVoisin.ID_MODELE_TERRAIN,1);
+                    }                       
+                }
+            }
+            Monitor.Exit(Donnees.m_donnees.TAB_MODELE_TERRAIN);
 
-               //maintenant on affecte le modèle le plus souvent rencontré
-               if (0 == liste.Count)
-               {
-                   //le terrain n'est entouré que par des terrains eux aussi modifiables, tant pis, on garde le même (cela n'arrivera pas souvent !)
-                   ligneCase.ID_MODELE_TERRAIN_SI_OCCUPE = ligneCase.ID_MODELE_TERRAIN;
-               }
-               else
-               {
-                   maximum = 0;
-                   modeleCible = -1;
-                   foreach (Int32 clef in liste.Keys)
-                   {
-                       liste.TryGetValue(clef, out valeur);
-                       if (valeur > maximum)
-                       {
-                           maximum = valeur;
-                           modeleCible = clef;
-                       }
-                   }
-                   ligneCase.ID_MODELE_TERRAIN_SI_OCCUPE = modeleCible;
-               }
+            //maintenant on affecte le modèle le plus souvent rencontré
+            Monitor.Enter(m_tableCaseTraitement);
+            if (0 == liste.Count)
+            {
+                //le terrain n'est entouré que par des terrains eux aussi modifiables, tant pis, on garde le même (cela n'arrivera pas souvent !)
+                ligneCase.ID_MODELE_TERRAIN_SI_OCCUPE = ligneCase.ID_MODELE_TERRAIN;
+            }
+            else
+            {
+                maximum = 0;
+                modeleCible = -1;
+                foreach (Int32 clef in liste.Keys)
+                {
+                    liste.TryGetValue(clef, out valeur);
+                    if (valeur > maximum)
+                    {
+                        maximum = valeur;
+                        modeleCible = clef;
+                    }
+                }
+                ligneCase.ID_MODELE_TERRAIN_SI_OCCUPE = modeleCible;
+            }
+            Monitor.Exit(m_tableCaseTraitement);
         }
 
         /// <summary>
@@ -587,7 +682,9 @@ namespace vaoc
             
             //Mise a jour de la case
             Donnees.TAB_CASERow ligneCase=m_tableCase.FindByXY(x,y);
-            ligneCase.ID_MODELE_TERRAIN=resModeleTerrain[0].ID_MODELE_TERRAIN;
+            Monitor.Enter(m_tableCase);
+            ligneCase.ID_MODELE_TERRAIN = resModeleTerrain[0].ID_MODELE_TERRAIN;
+            Monitor.Exit(m_tableCase);
             return true;
         }
 

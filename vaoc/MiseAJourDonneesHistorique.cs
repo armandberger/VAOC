024949d2784d2900be7d774c85c0a5c83bb4ddb2
armandberger <hoel@free.fr>
2017-02-11 18:23:33 +0100

    fichier++

---
+++ vaoc/MiseAJourDonneesHistorique.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaoc
+{
+    /// <summary>
+    /// Mise à jour de données sur tous les fichiers depuis le début de la campagne
+    /// </summary>
+    class MiseAJourDonneesHistorique
+    {
+    }
+}

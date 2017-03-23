371deb9d14ccc4cbf04bf6edd6533193701c3925
Armand <hoel@free.fr>
2017-03-23 06:29:11 +0100

    Traitement batch

---
+++ Traitement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Traitement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap imageSource = (Bitmap)Bitmap.FromFile("C:\\Users\\Public\\Documents\\vaoc\\1813\\1813_zoom_final.bmp");
+
+            BitmapData imageCible = new BitmapData();
+            imageSource.LockBits(new Rectangle(100,100,200,100), ImageLockMode.ReadOnly, imageSource.PixelFormat, imageCible);
+            imageSource.UnlockBits(imageCible);
+            Bitmap imageFinale = new Bitmap(imageCible.Width, imageCible.Height, imageCible.Stride, imageCible.PixelFormat, imageCible.Scan0);
+            imageFinale.Save("C:\\Users\\Public\\Documents\\vaoc\\1813\\1813P_zoom_final.bmp", ImageFormat.Bmp);
+        }
+    }
+}
---
+++ Traitement/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Traitement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Traitement")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("79d0ad6f-d240-458d-98c1-0c0204031b7d")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
